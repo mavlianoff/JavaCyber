a4faa4175023f26a55146caed0776318af0382d2
User <User@DESKTOP-F93OHP7>
2021-04-18 15:35:24 +0200

    Lab2_Q1_Solution is uploaded

--- javaprj1/src/day18_readingUserInput/lab2_Q1_LargestSmallest.java
+++ javaprj1/src/day18_readingUserInput/lab2_Q1_LargestSmallest.java
@@ -6,27 +6,49 @@ public class lab2_Q1_LargestSmallest {
 	
 	public static void main(String[] args) {
 		
-		Scanner scan=new Scanner(System.in);
-		System.out.print("Enter your first number: ");
-		int firstNumber = scan.nextInt();
-		System.out.print("Enter your next number: ");
-		int nextNumber = scan.nextInt();
+		/*
+		Write a program with a loop that lets the user enter a series of integer numbers. 
+		After all the numbers have been entered, the program should display the largest and smallest numbers entered.
+		 */
 		
-		do {
-			System.out.print("Do you want to enter another number?: 0-No, 1-Yes");
-			int confirmAnotherNumber=scan.nextInt();
+		Scanner sc= new Scanner(System.in);
+		
+		
+		System.out.print("Enter your first number:");
+		int num=sc.nextInt();
+		int small=num, largest=num;
+		//boolean i=true;
+		while (true) {
+			System.out.print("Enter your next number:");
+			int num1=sc.nextInt();
 			
-			if(confirmAnotherNumber==1) {
-				System.out.print("Enter your next number: ");
-				int anotherNumber = scan.nextInt();	
-			}else if(confirmAnotherNumber==0) {
-				break;
-			}else {
-				System.out.println("Invalid input");
-					break;
+			if(small>num1) {
+				small=num1;
+				
+			}else if (largest<num1){
+				largest=num1;
 			}
 			
-		}while(true);
+			System.out.print("Do you want to enter another number: 0-No, 1-Yes");
+			int num2=sc.nextInt();
+			
+			
+			if (num2==0)
+				//i=false;
+				break;
+			if (num2!=0&&num2!=1) {
+				System.out.println("Invalid Input, please select 0 or 1");
+				//continue;	
+				
+				System.out.println("Do you want to enter another number: 0-No, 1-Yes");
+				//int num2=sc.nextInt();
+				continue;
+			}
+		}
+
+		System.out.println("Smallest number is:" + small);
+		
+	    System.out.println("Largest number is:" + largest);
 	
 	}
 }
