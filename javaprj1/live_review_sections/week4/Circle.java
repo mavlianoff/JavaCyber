791c4e8bf110d2c3be4c8ee3dd0daf7e29e0a4d7
User <User@DESKTOP-F93OHP7>
2021-04-18 19:32:24 +0200

    Week4_LiveSession added

---
+++ javaprj1/live_review_sections/week4/Circle.java
@@ -0,0 +1,25 @@
+package week4;
+
+public class Circle {
+	
+	//Class has (1) Attributes and (2) Actions/Behaviours
+	
+	// (1)Circle attribute: RADIUS
+	public double radius; // public because it shall be used accross packages
+	
+	// (2) Actions/Behaviours: calculateArea, calculatePermieter
+	public double calculateArea() {			// instance variable -> multiple object (circle 1, circle , etc) uses it, therefore no 'STATIC'
+		return radius * radius * Math.PI;
+	}
+	
+	public double calculatePerimeter(){		// instance variable -> multiple object (circle 1, circle , etc) uses it, therefore no 'STATIC'
+		return radius * 2 * 3.14;
+	}
+	
+	public String toString() {
+		return "Radius " + radius
+				+ ", Area: " + calculateArea()
+				+ ", Peremeter: " + calculatePerimeter();
+	}
+
+}
---
+++ javaprj1/live_review_sections/week4/CircleObjects.java
@@ -0,0 +1,28 @@
+package week4;
+
+public class CircleObjects {
+	
+	public static void main(String[] args) {
+		
+		Circle circle1 = new Circle(); //created new object with method with same data type and assign to ref variable to be re-usable
+		
+		// We want to print newly created object with its attributed + behaviours.
+		System.out.println(circle1); 
+
+		
+		// PRIOR toString() method defined in class:
+		// Outcome: week4.Circle@26f0a63f - machine ca not read part: @26f0a63f
+		// toString() method is what you need to convert hash data
+		
+		// POST toString() method defined in class:
+		// Outcome: Radius 0.0, Area: 0.0, Peremeter: 0.0    ==> provided default values: 0.0
+		// toString() method is added to class without method name modification.
+		
+		System.out.println(circle1.toString()); // still, emthod has to be defined in class in order to use it here.
+		
+		// toString() ==> only ONE mission: to convert object to String
+									
+		
+	}
+
+}
