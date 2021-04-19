ca1609576a70bb98c55910a46befc799f9f88e1f
User <User@DESKTOP-F93OHP7>
2021-04-19 02:34:45 +0200

    Week4 Live session forked codes

---
+++ javaprj1/live_review_sections/utilities/StringUtility.java
@@ -0,0 +1,23 @@
+package utilities;
+
+public class StringUtility {
+	
+	
+	// This method can reverse any given string, and returns the reversed string 
+	public static String reverse(String str) {
+		String result = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) { // i: represents the index numbers starting from last index to 0
+			result += str.charAt(i);
+		}
+		
+		return result;
+	}
+	
+	
+	// THis method can verify if a give string is palindrome, returns true or false
+	public static boolean isPalindrome(String str) {
+		return  reverse(str).equalsIgnoreCase(str);
+	}
+	
+}
---
+++ javaprj1/live_review_sections/week4/Palindrome.java
@@ -0,0 +1,25 @@
+package week4;
+
+import utilities.StringUtility;
+
+public class Palindrome {
+	
+	public static void main(String[] args) {
+		
+		//"Anna"  ==> "annA"
+		//"Level" ==> "leveL"
+		
+		String word = "Anna";
+		boolean isPalindrome =  StringUtility.reverse(word).equalsIgnoreCase(word);
+		
+		System.out.println(isPalindrome);
+		
+		
+		System.out.println("----------------------------------");
+		
+		System.out.println( StringUtility.isPalindrome("Muhtar") );
+		
+		
+	}
+
+}
