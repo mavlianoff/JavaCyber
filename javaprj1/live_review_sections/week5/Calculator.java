87469240fdd204ce5c38c9a4814aed04b69c33d3
User <User@DESKTOP-F93OHP7>
2021-04-25 18:16:05 +0200

    while loop calculator

---
+++ javaprj1/live_review_sections/week5/Calculator.java
@@ -0,0 +1,42 @@
+package week5;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); // \n\n
+		
+		while(true) {
+			System.out.println("Please enter first number: ");
+			int f = scan.nextInt(); // 10\n ==> \n goes to scanner memory
+	
+			System.out.println("Please enter second number: ");
+			int s = scan.nextInt(); // 20\n ==> \n goes to scanner memory
+			
+			System.out.println("Sum: " + (f+s));
+			
+			scan.nextLine(); //  \n\n
+			
+			System.out.println("Do you want to continue?");
+			String a = scan.nextLine().toLowerCase();
+			
+			while( !(a.equals("yes") || a.equals("no")) ) {
+				System.out.println("Invalid entry, please re-enter");
+				a = scan.nextLine().toLowerCase();
+			}
+			
+			if(a.equals("no")) {
+				System.out.println("Test completed!");
+				break;
+			}
+		}
+		
+		
+		
+		
+
+	}
+
+}
