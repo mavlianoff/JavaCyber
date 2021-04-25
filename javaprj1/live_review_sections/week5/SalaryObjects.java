b36338f6775ceca945a3841ecbe349883477da7d
User <User@DESKTOP-F93OHP7>
2021-04-25 17:05:12 +0200

    Class salary()

--- javaprj1/live_review_sections/week5/SalaryObjects.java
+++ javaprj1/live_review_sections/week5/SalaryObjects.java
@@ -6,11 +6,15 @@ public class SalaryObjects {
 		
 		Salary salary1 = new Salary();
 //		System.out.println(salary1); //i can print it because i have set toString() method in class
-		salary1.setInfo(25, 40, 0.27);
+		salary1.setInfo(25, 40, 0.27); //employee1
 		System.out.println(salary1); //i can print it because i have set toString() method in class
 		
+		Salary salary2 = new Salary(); //employee2
+		salary2.setInfo(55, 45, 0.28);
+		System.out.println(salary2);
 		
-		
+		salary2.setInfo(65, 45, 0.28); //employee2 got promoted
+		System.out.println(salary2);
 		
 		
 		
