082df2f5eb69771afa22fd1421c44fa88d17e298
User <User@DESKTOP-F93OHP7>
2021-05-12 04:00:15 +0200

    day30_task98

--- javaprj1/src/day30_WrapperClass/task98_decodeTheCode.java
+++ javaprj1/src/day30_WrapperClass/task98_decodeTheCode.java
@@ -7,29 +7,62 @@ public class task98_decodeTheCode {
 		String str1 = "&^%^1&8O^^..ne. .52||{{T.*@,(wo# &%^f!@#$%$#$%^%$$%i45645756768ve$% $S#$%^..,,!i#@$^**&^x@!$";
 		String str2 = "N000000000000i@#$%n9@#$e!!!!!! 43.***T#@$hr@#$%##<e><e>>>>{{ ##f##00o00###u%$@##$%#r";
 		
-		System.out.println(decodingCode(str1, str2));
+		System.out.println(decodingCode(str1));
+		System.out.println(decodingCode(str2));
+		
 		
 	}
-	//INCOMPLETE:
-	public static String decodingCode(String strx, String stry) {
-		String newStrx = "";
-		String newStry = "";
+	
+	public static String decodingCode(String s) {
 		
-		for(int i=0; i<strx.length(); i++) {
-			if(Character.isAlphabetic(strx.charAt(i)) || strx.charAt(i)==' ') {
-				newStrx = newStrx + strx.charAt(i);
+		String str = "";
+		for(int i = 0; i < s.length(); i++) {
+			if( Character.isAlphabetic(s.charAt(i)) || s.charAt(i)==' ' ) {
+				str = str + s.charAt(i);
 			}
 		}
+		
+		String[] arr = str.split(" ");
+		
+		String strr = ""; // we shall concatenate numbers once it is converted to String[digits]
+		
+		for(String value: arr) {
+			strr = strr + getDigit(value) + " ";
+		}
+		
+		
+		
+		 return strr;
+	}
+
+	public static String getDigit(String st) {
+		
+		switch(st.toUpperCase()){
+		case "ZERO":
+			return "0";
+		case "ONE":
+			return "1";
+		case "TWO":
+			return "2";
+		case "THREE":
+			return "3";
+		case "FOUR":
+			return "4";
+		case "FIVE":
+			return "5";
+		case "SIX":
+			return "6";
+		case "SEVEN":
+			return "7";
+		case "EIGHT":
+			return "8";
+		case "NINE":
+			return "9";
+		default:
+			return "";
 			
-		for(int i=0; i<stry.length(); i++) {
-			if(Character.isAlphabetic(stry.charAt(i)) || stry.charAt(i)==' ') {
-				newStry = newStry + stry.charAt(i);
-			}
 		}
-		
-		return newStrx;
-		//return newStry;
-		
+
 	}
 }
 /*
