a302e7f9fbc7cc81d19c226fe15813e6b93bb100
smavlianoff <m.sanzharbek@gmail.com>
2021-06-08 00:06:27 +0200

    week11

---
+++ javaprj1/live_review_sections/week11/Square.java
@@ -0,0 +1,34 @@
+package week11;
+
+import java.text.DecimalFormat;
+
+public class Square extends Shape{
+	
+	public double side;
+
+	public Square(double side) {
+		super("Square");
+		this.side = side;
+	}
+
+	@Override
+	public double area() {
+		return side * side;
+	}
+
+	@Override
+	public double perimeter() {
+		return 4*side;
+	}
+
+	@Override
+	public String toString() {
+		DecimalFormat df = new DecimalFormat("0.00");
+
+		return "Square [side= " + side 
+				+ ", area= " + df.format( area() )
+				+ ", perimeter)=" +  df.format( perimeter() ) + "]";
+	}
+	
+
+}
